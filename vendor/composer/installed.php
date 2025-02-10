3c1ce5450204e07a51d3f4533d88ad7c08f44edd
Laxman Adhikari <54370052+prolaxu@users.noreply.github.com>
2025-02-10 22:07:54 +0545

    clean up

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '24834c14776941663e6b980b6d6b960dc31dd672',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -446,9 +446,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '24834c14776941663e6b980b6d6b960dc31dd672',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
